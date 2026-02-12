01c172a23bd3850787290f97521238bf764ed6e1
Dhyanesa <adhyanesa@gmail.com>
2026-02-12 14:57:53 +0700

    Add files via upload

---
+++ KodePertama.java
@@ -0,0 +1,12 @@
+public class KodePertama{
+    public static void main(String[] args) {
+        System.out.println("Halo, Pemrograman Berorientasi Objek!");
+        System.out.println("Ini adalah program Java pertama saya.");
+    }
+}
+
+/*
+Nama : Dhyanesa Amirah Kanandhi
+NIM : D1041241011
+Tanggal : 5 Februari 2026
+ */
---
+++ KodePertamaLatihan.java
@@ -0,0 +1,14 @@
+public class KodePertamaLatihan {
+    public static void main(String[] args) {
+        System.out.println("Nama: Dhyanesa Amirah Kanandhi");
+        System.out.println("NIM: D1041241011");
+        System.out.println("Semester: 4");
+        System.out.println("Prodi: Informatika");
+    }
+}
+
+/*
+Nama : Dhyanesa Amirah Kanandhi
+NIM : D1041241011
+Tanggal : 5 Februari 2026
+ */
