a2c7ad2663a3e2129858aaeb126a03be9f4bfcfa
Dhyanesa <adhyanesa@gmail.com>
2026-02-12 15:07:24 +0700

    Add files via upload

---
+++ p022WorkingVariable.java
@@ -0,0 +1,21 @@
+public class p022WorkingVariable {
+    public static void main(String[] args) {
+        //deklarasi dan inisialisasi variabel
+        int angka = 10;
+
+        //Menampilkan nilai awal variabel
+        System.out.println("Nilai awal angka: " + angka);
+
+        //Mengubah nilai variabel
+        angka = 25;
+
+        //Menampilkan nilai setelah perubahan
+        System.out.println("Nilai angka setelah diubah: " + angka);
+    }
+}
+
+/*
+Nama : Dhyanesa Amirah Kanandhi
+NIM : D1041241011
+Tanggal : 12 Februari 2026
+ */
---
+++ p023PrimitiveData.java
@@ -0,0 +1,32 @@
+public class p023PrimitiveData {
+    public static void main(String[] args){
+        //Tipe data bilangan bulat
+        int umur = 20;
+        long populasi = 8000000000L;
+
+        //Tipe data bilangan desimal
+        float tinggi = 170.5f;
+        double ipk = 3.75;
+
+        //Tipe data karakter
+        char nilaihuruf = 'A';
+
+        //Tipe data logika
+        boolean lulus = true;
+
+        //Menampilkan nilai semua variabel
+        System.out.println("Umur           : " + umur);
+        System.out.println("Populasi       : " + populasi);
+        System.out.println("Tinggi Badan   : " + tinggi);
+        System.out.println("IPK            : " + ipk);
+        System.out.println("Nilai Huruf    : " + nilaihuruf);
+        System.out.println("Status Lulus   : " + lulus);
+
+    }
+}
+
+/*
+Nama : Dhyanesa Amirah Kanandhi
+NIM : D1041241011
+Tanggal : 12 Februari 2026
+*/
