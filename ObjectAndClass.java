f4fce85736e12c77a8627c596600c863d72f2222
Dhyanesa <adhyanesa@gmail.com>
2026-02-20 09:19:20 +0700

    Add files via upload

---
+++ ObjectAndClass.java
@@ -0,0 +1,30 @@
+class ObjectClass {
+    String merk;
+    String warna;
+    int tahun;
+    int kecepatan;
+
+    public void percepatan(int increment){
+        kecepatan += increment;
+    }
+
+    public void tampilkanInfo() {
+        System.out.println("merk : " + merk);
+        System.out.println("warna : " + warna);
+        System.out.println("tahun : " + tahun);
+        System.out.println("kecepatan : " + kecepatan + " km/h ");
+    }
+}
+
+public class ObjectAndClass {
+    public static void main(String[] args){
+        ObjectClass mobil1 = new ObjectClass();
+        mobil1.merk = "Wuling";
+        mobil1.warna = "ungu";
+        mobil1.tahun = 2026;
+        mobil1.kecepatan = 70;
+
+        System.out.println("Informasi Mobil:");
+        mobil1.tampilkanInfo();
+    }
+}
