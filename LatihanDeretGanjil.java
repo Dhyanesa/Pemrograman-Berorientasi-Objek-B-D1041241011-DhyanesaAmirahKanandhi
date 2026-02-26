e38813a358a66f8ce393caf2a82c17fc23843af7
Dhyanesa <adhyanesa@gmail.com>
2026-02-26 17:06:35 +0700

    Add files via upload

---
+++ LatihanDeretGanjil.java
@@ -0,0 +1,20 @@
+public class LatihanDeretGanjil {
+    public static void main(String[] args) {
+        int angka = 1;
+
+        System.out.println("=== BILANGAN GANJIL 1-15 ===");
+
+        while (angka <= 15) {
+            if (angka % 2 != 0) {
+                System.out.print(angka + " ");
+            }
+            angka++;
+        }
+        System.out.println();
+    }
+}
+/*
+Nama : Dhyanesa Amirah Kanandhi
+NIM : D1041241011
+Kelas : B
+ */
