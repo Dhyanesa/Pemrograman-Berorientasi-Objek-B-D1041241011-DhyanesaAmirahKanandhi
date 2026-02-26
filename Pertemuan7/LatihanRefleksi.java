879652fe1d42677750bb41905e5f2335cbc92a6d
Dhyanesa <adhyanesa@gmail.com>
2026-02-26 17:09:57 +0700

    Menambahkan LatihanRefleksi pada pertemuan 7

---
+++ Pertemuan7/LatihanRefleksi.java
@@ -0,0 +1,30 @@
+public class LatihanRefleksi {
+    public static void main(String[] args) {
+
+        System.out.println("=== MENGGUNAKAN WHILE ===");
+        int i = 1;
+        while (i <= 5) {
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.println("\n");
+
+        System.out.println("=== MENGGUNAKAN DO-WHILE ===");
+        int j = 1;
+        do {
+            System.out.print(j + " ");
+            j++;
+        } while (j <= 5);
+        System.out.println("\n");
+        System.out.println("=== MENGGUNAKAN FOR ===");
+        for (int k = 1; k <= 5; k++) {
+            System.out.print(k + " ");
+        }
+        System.out.println();
+    }
+}
+/*
+Nama : Dhyanesa Amirah Kanandhi
+NIM : D1041241011
+Kelas : B
+ */
