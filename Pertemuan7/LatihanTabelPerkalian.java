a072855c988dc4ae97f41b5fb338aa939846ad19
Dhyanesa <adhyanesa@gmail.com>
2026-02-26 17:19:17 +0700

    Menambahkan LatihanTabelPerkalian pada pertemuan 7

---
+++ Pertemuan7/LatihanTabelPerkalian.java
@@ -0,0 +1,17 @@
+public class LatihanTabelPerkalian {
+    public static void main(String[] args) {
+        int angka = 7;
+
+        System.out.println("=== TABEL PERKALIAN " + angka + " ===");
+
+        for (int i = 1; i <= 12; i++) {
+            int hasil = angka * i;
+            System.out.println(angka + " x " + i + " = " + hasil);
+        }
+    }
+}
+/*
+Nama : Dhyanesa Amirah Kanandhi
+NIM : D1041241011
+Kelas : B
+ */
