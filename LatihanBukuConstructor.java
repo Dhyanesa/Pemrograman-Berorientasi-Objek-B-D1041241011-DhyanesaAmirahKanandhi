5841ca2ba98e59c1627bc89294207a2923940a0b
Dhyanesa <adhyanesa@gmail.com>
2026-03-11 23:10:40 +0700

    Add files via upload

---
+++ LatihanBukuConstructor.java
@@ -0,0 +1,42 @@
+class Buku {
+    String judul;
+    String pengarang;
+    int harga;
+
+    public Buku() {
+        this.judul = "Belum diisi";
+        this.pengarang = "Belum diisi";
+        this.harga = 0;
+    }
+
+    public Buku(String judul, String pengarang, int harga) {
+        this.judul = judul;
+        this.pengarang = pengarang;
+        this.harga = harga;
+    }
+
+    public void tampilkanInfo() {
+        System.out.println("=== INFORMASI BUKU ===");
+        System.out.println("Judul    : " + judul);
+        System.out.println("Pengarang: " + pengarang);
+        System.out.println("Harga    : Rp " + harga);
+    }
+}
+
+public class LatihanBukuConstructor {
+    public static void main(String[] args) {
+        Buku buku1 = new Buku();
+
+        Buku buku2 = new Buku("Pemrograman Java", "Budi Raharjo", 125000);
+        Buku buku3 = new Buku("Algoritma dan Struktur Data", "Rinaldi Munir", 150000);
+
+        buku1.tampilkanInfo();
+        buku2.tampilkanInfo();
+        buku3.tampilkanInfo();
+    }
+}
+/*
+Nama : Dhyanesa Amirah Kanandhi
+NIM : D1041241011
+Kelas : B
+ */
